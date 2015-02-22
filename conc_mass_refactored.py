8e8a4f08554c42fafa817f3d3fa7b4e9cc31a3f5
Andrew Hearin <andrew.hearin@yale.edu>
2015-02-22 13:25:17 -0500

    Added docstrings

--- conc_mass_refactored.py
+++ conc_mass_refactored.py
@@ -21,7 +21,6 @@ def conc_mass_orig(loglogmass, modelname):
 	return conc
 
 
-
 ###########################################################################################
 # Refactored version below
 ###########################################################################################
@@ -29,6 +28,19 @@ def conc_mass_orig(loglogmass, modelname):
 
 #####################  SOME SPECIFIC MODELS #####################
 def conc_mass_dutton_macchio14(logmass):
+	""" Concentration-mass model calibrated in Dutton & Macchio (2014). 
+
+	Parameters 
+	----------
+	logmass : array 
+		Halo masses, specified in logarithmic units. 
+
+	Returns 
+	-------
+	conc : array 
+		Halo concentrations. 
+	"""
+
 	conc = np.where(logmass < 10, 4, logmass**0.445)
 	return conc
 
@@ -37,30 +49,34 @@ def conc_mass_whatever(logmass):
 
 #####################
 
-def retrieve_model(modelname, testmode=False):
-	""" Function whose sole purpose is to retrieve the requested model
-	"""
-	default_model = conc_mass_dutton_macchio14
-
-	if modelname=='dutton_macchio14': 
-		return conc_mass_dutton_macchio14
-	elif modelname=='whatever':
-		return conc_mass_whatever
-	elif modelname==None:
-		msg = "No modelname provided. Choosing default = "+default_model.__name__
-		if testmode==True: print(msg)
-		return default_model
-	else:
-		msg = ("Input modelname does not correspond to \n"
-			"any of the supported model names, which are "
-			"'dutton_macchio14' and 'whatever'\n"
-			"Choosing "+default_model.__name__+" as default")
-		warn(msg)
-		return default_model
-
-##################### PRIMARY METHOD #####################
+####################################################################
+############## PRIMARY METHOD CALLED BY OUTSIDE WORLD ##############
 
 def conc_mass(logmass, modelname=None, testmode=False):
+	""" Function specifying the relationship between halo mass and 
+	NFW profile concentration. Multiple models are supported, including 
+	Dutton & Macchio (2014), and whatever. 
+
+	Parameters 
+	----------
+	logmass : array 
+		Halo masses, specified in logarithmic units. 
+
+	modelname : string, optional 
+		String used to specify which model is chosen. 
+		If None, the default model will be selected.
+		The default model is set in the retrieve_model function. 
+
+	testmode : bool, optional 
+		If set to True, the code will run in Test Mode, and many sanity 
+		checks will be performed while the functions evaluate. 
+
+	Returns 
+	-------
+	conc : array 
+		Halo concentrations. 
+
+	"""
 
 	# First make sure the input is a numpy type object
 	# If not, convert it. 
@@ -78,5 +94,45 @@ def conc_mass(logmass, modelname=None, testmode=False):
 	conc_function = retrieve_model(modelname, testmode)
 
 	return conc_function(logmass)
+
+####################################################################
+
+def retrieve_model(modelname, testmode=False):
+	""" Function whose sole purpose is to retrieve the requested model. 
+
+	Parameters 
+	----------
+	modelname : string 
+
+	testmode : bool, optional 
+		If set to True, the code will run in Test Mode, and many sanity 
+		checks will be performed while the functions evaluate. 
+
+	Returns 
+	-------
+	model : function object 
+
+	"""
+	default_model = conc_mass_dutton_macchio14
+
+	if modelname=='dutton_macchio14': 
+		return conc_mass_dutton_macchio14
+
+	elif modelname=='whatever':
+		return conc_mass_whatever
+
+	elif modelname==None:
+		msg = "No modelname provided. Choosing default = "+default_model.__name__
+		if testmode==True: print(msg)
+		return default_model
+
+	else:
+		msg = ("Input modelname does not correspond to \n"
+			"any of the supported model names, which are "
+			"'dutton_macchio14' and 'whatever'\n"
+			"Choosing "+default_model.__name__+" as default")
+		warn(msg)
+		return default_model
+
 
 
--- test_helpers.py
+++ test_helpers.py
@@ -6,8 +6,31 @@ from warnings import warn
 
 def test_sensible_values(array, minval, maxval, 
 	called_funcname=None, array_name=''):
-	""" Helper function for testing. 
+	""" Helper function for testing whether some other, external function 
+	was passed a reasonable set of inputs. 
+
+	Parameters 
+	----------
+	array : array 
+		Input set of values to the external function. 
+
+	minval : float or int 
+		Minimum value that would have been reasonable to pass to the external function. 
+
+	maxval : float or int 
+		Maximum value that would have been reasonable to pass to the external function. 
+
+	called_funcname : string, optional 
+		Name of the external function whose inputs are being tested. 
+		Provides a convenient way to quickly zero in on potential bugs by helping 
+		to construct an informative error and/or warning message. 
+
+	array_name : string, optional 
+		Variable name of the array whose values are being tested. 
+		Provides a convenient way to quickly zero in on potential bugs by helping 
+		to construct an informative error and/or warning message. 
 	"""
+	
 	msg = ("at least one element of the input array "+array_name+"\n" + 
 		"is outside the reasonable range of %.1f to %.1f \n")
 	if called_funcname != None:
@@ -17,20 +40,43 @@ def test_sensible_values(array, minval, maxval,
 		raise ValueError(msg % (minval, maxval))
 
 def get_array_like(x, array_name='', called_funcname=None):
+	""" Function that converts the input x to a numpy array. 
+
+	Parameters 
+	----------
+	x : object 
+		Can be any input that is desired to be converted. 
+
+	array_name : string, optional 
+		Variable name of the array to be converted. 
+		Provides a convenient way to quickly zero in on potential bugs by helping 
+		to construct an informative error and/or warning message. 
+
+	Returns 
+	-------
+	y : array 
+		Numpy array version of input x. 
+	"""
+
 	types_to_convert = [list, tuple, float, int]
 
 	if type(x).__module__ == np.__name__:
 		return x
+
 	elif type(x) in types_to_convert:
 		msg = "Array "+array_name+" should not be list or tuple\n"+"Converting to ndarray"
 		if called_funcname != None:
 			msg = msg + "\nIn the future, use ndarray inputs for "+called_funcname
-		msg = msg + "\nThis message will self-destruct\n"
+		msg = msg + "\nThis message will not be displayed in the future\n"
 		if type(x)==list or type(x)==tuple: warn(msg)
 		return np.array(x)
+
 	else:
 		msg = "Array "+array_name+" has an insensible type = "+str(type(x))+"\n"
 		"Should be ndarray, list, tuple, float_like, or int_like"
 		if called_funcname!= None:
 			msg = msg + "\n when calling function "+called_funcname
 		raise TypeError(msg)
+
+
+
